033977cb09969685149aa81fda9bbea5f26b6682
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-05-18 09:34:41 -0500

    DEVX-1524 working start-docker-cloud snapshot

--- cp-quickstart/start-docker-cloud.sh
+++ cp-quickstart/start-docker-cloud.sh
@@ -5,9 +5,10 @@
 # To avoid unexpected charges, carefully evaluate the cost of resources before launching the script and ensure all resources are destroyed after you are done running it.
 #########################################
 
-
 # Source library
 source ../utils/helper.sh
+
+NAME=`basename "$0"`
 
 check_jq \
   && print_pass "jq found"
@@ -22,13 +23,17 @@ print_pass "Prerequisite check pass"
 
 wget -q -O docker-compose.yml https://raw.githubusercontent.com/confluentinc/cp-all-in-one/${CONFLUENT_RELEASE_TAG_OR_BRANCH}/cp-all-in-one-cloud/docker-compose.yml \
   && print_pass "retrieved docker-compose.yml from https://github.com/confluentinc/cp-all-in-one" \
-  || exit_with_error -c $? -n "start-docker-cloud.sh" -m "could not obtain cp-all-in-one/docker-compose.yml" -l $(($LINENO -2))
+  || exit_with_error -c $? -n "$NAME" -m "could not obtain cp-all-in-one/docker-compose.yml" -l $(($LINENO -2))
 
-printf "\n====== Confirm\n"
-prompt_continue_cloud_demo || exit 1
-read -p "Do you acknowledge this script creates a Confluent Cloud KSQL app (hourly charges may apply)? [y/n] " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
- 
+[[ -z "$AUTO" ]] && {
+  printf "\n====== Confirm\n"
+  prompt_continue_cloud_demo || exit 1
+  read -p "Do you acknowledge this script creates a Confluent Cloud KSQL app (hourly charges may apply)? [y/n] " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
+} 
+
+printf "\nFor your reference the important commands that are executed will appear in "; print_code "code format"
+
 printf "\n\n====== Creating new Confluent Cloud stack\n"
 cloud_create_demo_stack true
 
@@ -42,62 +47,115 @@ source $DELTA_CONFIGS_DIR/env.delta
 
 # Pre-flight check of Confluent Cloud credentials specified in $CONFIG_FILE
 MAX_WAIT=720
-echo "Waiting up to $MAX_WAIT seconds for Confluent Cloud KSQL cluster to be UP"
+print_process_start "Waiting up to $MAX_WAIT seconds for Confluent Cloud KSQL cluster to be UP"
 retry $MAX_WAIT check_ccloud_ksql_endpoint_ready $KSQL_ENDPOINT || exit 1
 print_pass "Confluent Cloud KSQL is UP"
 ccloud_demo_preflight_check $CLOUD_KEY $CONFIG_FILE || exit 1
+
+print_process_start "Pre-creating topics"
+
+CMD="ccloud kafka topic create _confluent-monitoring"
+$CMD \
+  && print_code_pass -c "$CMD" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3))
+
+CMD="ccloud kafka topic create pageviews"
+$CMD \
+  && print_code_pass -c "$CMD" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3)) 
+
+CMD="ccloud kafka topic create pageviews_regions"
+$CMD  \
+  && print_code_pass -c "$CMD" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3))
+
+CMD="ccloud kafka topic create users"
+$CMD \
+  && print_code_pass -c "$CMD" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3))
+
+CMD="ccloud kafka topic create accomplished_female_readers"
+$CMD \
+  && print_code_pass -c "$CMD" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3))
+
+print_pass "Pre-created topics"
  
-ccloud kafka topic create _confluent-monitoring \
-  || exit_with_error -c $? -n "start-docker-cloud.sh" -m "could not create _confluent-monitoring topic" -l $(($LINENO -1))
-ccloud kafka topic create pageviews \
-  || exit_with_error -c $? -n "start-docker-cloud.sh" -m "could not create pageviews topic" -l $(($LINENO -1))
-ccloud kafka topic create users \
-  || exit_with_error -c $? -n "start-docker-cloud.sh" -m "could not create users topic" -l $(($LINENO -1))
-print_pass "Pre-created topics\n"
- 
-printf "\nStarting local connect cluster to generate simulated data\n"
-docker-compose up -d connect
+printf "\nStarting local connect cluster in Docker to generate simulated data\n"
+CMD="docker-compose up -d connect"
+$CMD \
+  && print_code_pass -c "$CMD" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -2))
 
 # Verify Kafka Connect worker has started
 MAX_WAIT=120
-printf "\nWaiting up to $MAX_WAIT seconds for Kafka Connect to start\n"
+printf "\n"
+print_process_start "Waiting up to $MAX_WAIT seconds for Kafka Connect to start"
 retry $MAX_WAIT check_connect_up connect || exit 1
 sleep 2 # give connect an exta moment to fully mature
-print_pass "Kafka Connect has started\n"
+print_pass "Kafka Connect has started"
  
 # Configure datagen connectors
-source ./connectors/submit_datagen_pageviews_config_cloud.sh
-source ./connectors/submit_datagen_users_config_cloud.sh
+source ./connectors/submit_datagen_pageviews_config_cloud.sh >/dev/null 2>&1
+source ./connectors/submit_datagen_users_config_cloud.sh >/dev/null 2>&1
 
 printf "\nSleeping 30 seconds to give kafka-connect-datagen a chance to start producing messages\n"
 sleep 30
 
-# Run the KSQL queries
-printf "\nSubmit KSQL queries\n"
-ksqlAppId=$(ccloud ksql app list -o json | jq -r '.[].id')
-ccloud ksql app configure-acls $ksqlAppId pageviews users PAGEVIEWS_FEMALE pageviews_female_like_89 PAGEVIEWS_REGIONS
+printf "\nSetting up ksqlDB\n"
 
-properties='"ksql.streams.auto.offset.reset":"earliest","ksql.streams.cache.max.bytes.buffering":"0"'
-while read ksqlCmd; do # from statements.sql
-  response=$(curl -s -w "\n%{http_code}" -X POST $KSQL_ENDPOINT/ksql \
+printf "Obtaining the ksqlDB App Id\n"
+CMD="ccloud ksql app list -o json | jq -r '.[].id'"
+ksqlAppId=$(eval $CMD) \
+  && print_code_pass -c "$CMD" -m "$ksqlAppId" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3))
+
+printf "\nConfiguring ksqlDB ACLs\n"
+CMD="ccloud ksql app configure-acls $ksqlAppId pageviews users pageviews_regions accomplished_female_readers PAGEVIEWS_FEMALE pageviews_female_like_89"
+$CMD \
+  && print_code_pass -c "$CMD" \
+  || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3))
+
+sleep 60
+printf "\nSubmitting KSQL queries\n\n"
+
+while read ksqlCmd; do # from docker-cloud-statements.sql
+
+echo -e "\n$ksqlCmd\n"
+  response=$(curl -X POST $KSQL_ENDPOINT/ksql \
        -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" \
        -u $KSQL_BASIC_AUTH_USER_INFO \
+       --silent \
        -d @<(cat <<EOF
-         {
-           "ksql": "$ksqlCmd",
-           "streamsProperties": {$properties}
-         }
+{
+  "ksql": "$ksqlCmd",
+  "streamsProperties": {$properties}
+}
 EOF
-        )) || exit_with_error -n "start-docker-cloud.sh" -m "curl command failure, reference libcurl errors" -c $? -l $((LINENO -9));
-  echo "$response" | {
-    read body
-    read code
-    if [[ "$code" -gt 299 ]];
-      then print_error "$(echo $body | jq .message)"
-      else print_pass "$ksqlCmd"
-    fi
-  }
-done < statements.sql
+))
+  echo $response
+  if [[ ! "$response" =~ "SUCCESS" ]]; then
+    echo -e "\nWARN: KSQL command '$ksqlCmd' did not include \"SUCCESS\" in the response. Please troubleshoot."
+  fi
+
+#CMD="curl -s -w \"\n%{http_code}\" -X POST $KSQL_ENDPOINT/ksql -H \"Content-Type: application/vnd.ksql.v1+json; charset=utf-8\" -u $KSQL_BASIC_AUTH_USER_INFO -d @"<
+#   response=$(eval $CMD) \
+#     && print_code_pass -c "$CMD" \
+#     || exit_with_error -c $? -n "$NAME" -m "curl command failure, reference libcurl errors" -l $((LINENO -2));
+# 
+#   BODY="{\\\"ksql\\\":\\\"$ksqlCmd\\\",\\\"streamsProperties\\\":{\\\"ksql.streams.auto.offset.reset\\\":\\\"earliest\\\",\\\"ksql.streams.cache.max.bytes.buffering\\\":\\\"0\\\"}}"
+#   printf "\ncurl command succeeed\nhere is the submitted KSQL command and REST API response:\n\n"
+#   echo "$response" | {
+#     read body
+#     read code
+#     if [[ "$code" -gt 299 ]];
+#       then print_code_error -c "$ksqlCmd" -m "$(echo "$body" | jq .message)"
+#       else print_code_pass  -c "$ksqlCmd" -m "$(echo "$body" | jq -r .[].commandStatus.message)"
+#     fi
+#   }
+#   printf "\n"
+#   sleep 5
+done < docker-cloud-statements.sql
 echo "-----------------------------------------------------------"
 
 echo
