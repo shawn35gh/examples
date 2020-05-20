977ccdd34700089ebcab62474728d19ee2eb1e59
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-05-20 07:55:55 -0500

    DEVX-1524 working start-docker-cloud snapshot

--- cp-quickstart/start-docker-cloud.sh
+++ cp-quickstart/start-docker-cloud.sh
@@ -5,10 +5,13 @@
 # To avoid unexpected charges, carefully evaluate the cost of resources before launching the script and ensure all resources are destroyed after you are done running it.
 #########################################
 
+NAME=`basename "$0"`
+LOG_FILE="$NAME.log"
+
+printf "\n>>>>>>>>>> starting %s @ %s >>>>>>>>>>\n\n" $NAME "$(date)" >> "$LOG_FILE"
+
 # Source library
 source ../utils/helper.sh
-
-NAME=`basename "$0"`
 
 check_jq \
   && print_pass "jq found"
@@ -21,38 +24,48 @@ check_ccloud_logged_in \
 
 print_pass "Prerequisite check pass"
 
-wget -q -O docker-compose.yml https://raw.githubusercontent.com/confluentinc/cp-all-in-one/${CONFLUENT_RELEASE_TAG_OR_BRANCH}/cp-all-in-one-cloud/docker-compose.yml \
-  && print_pass "retrieved docker-compose.yml from https://github.com/confluentinc/cp-all-in-one" \
-  || exit_with_error -c $? -n "$NAME" -m "could not obtain cp-all-in-one/docker-compose.yml" -l $(($LINENO -2))
-
 [[ -z "$AUTO" ]] && {
-  printf "\n====== Confirm\n"
+  printf "\n====== Confirm\n\n"
   prompt_continue_cloud_demo || exit 1
   read -p "Do you acknowledge this script creates a Confluent Cloud KSQL app (hourly charges may apply)? [y/n] " -n 1 -r
   if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
+	printf "\n"
 } 
 
-printf "\nFor your reference the important commands that are executed will appear in "; print_code "code format"
+printf "\nThis demo writes a log file located at:\n\t%s\n" "$(cd $(dirname $LOG_FILE);echo $PWD)/$LOG_FILE"
+printf "\nFor your reference important commands that are executed will appear in "; print_code "code format"; printf "For example:\n"
+print_code_pass -c "echo \"Hello Apache Kafka\""
 
-printf "\n\n====== Creating new Confluent Cloud stack\n"
-cloud_create_demo_stack true
+printf "\n====== Starting\n"
+
+(wget -q -O docker-compose.yml https://raw.githubusercontent.com/confluentinc/cp-all-in-one/${CONFLUENT_RELEASE_TAG_OR_BRANCH}/cp-all-in-one-cloud/docker-compose.yml | tee -a $LOG_FILE) \
+  && print_pass "retrieved docker-compose.yml from https://github.com/confluentinc/cp-all-in-one" \
+  || exit_with_error -c $? -n "$NAME" -m "could not obtain cp-all-in-one/docker-compose.yml" -l $(($LINENO -2))
+
+printf "\n====== Creating new Confluent Cloud stack using the cloud_create_demo_stack function\nSee: %s for details\n" "https://github.com/confluentinc/examples/blob/$CONFLUENT_RELEASE_TAG_OR_BRANCH/utils/helper_cloud.sh"
+(cloud_create_demo_stack true | tee -a $LOG_FILE) \
+	&& print_code_pass -c "ccloud_create_demo_stack true"
 
 SERVICE_ACCOUNT_ID=$(ccloud kafka cluster list -o json | jq -r '.[0].name' | awk -F'-' '{print $4;}')
 CONFIG_FILE=stack-configs/java-service-account-$SERVICE_ACCOUNT_ID.config
 export CONFIG_FILE=$CONFIG_FILE
-check_ccloud_config $CONFIG_FILE || exit 1
-../ccloud/ccloud-generate-cp-configs.sh $CONFIG_FILE
+(check_ccloud_config $CONFIG_FILE | tee -a $LOG_FILE) || exit 1
+
+(../ccloud/ccloud-generate-cp-configs.sh $CONFIG_FILE | tee -a $LOG_FILE) \
+	&& print_code_pass -c "../ccloud/ccloud-generate-cp-configs.sh $CONFIG_FILE"
+
 DELTA_CONFIGS_DIR=delta_configs
 source $DELTA_CONFIGS_DIR/env.delta
 
 # Pre-flight check of Confluent Cloud credentials specified in $CONFIG_FILE
 MAX_WAIT=720
-print_process_start "Waiting up to $MAX_WAIT seconds for Confluent Cloud KSQL cluster to be UP"
+printf "\n";print_process_start "Waiting up to $MAX_WAIT seconds for Confluent Cloud KSQL cluster to be UP"
 retry $MAX_WAIT check_ccloud_ksql_endpoint_ready $KSQL_ENDPOINT || exit 1
 print_pass "Confluent Cloud KSQL is UP"
+
 ccloud_demo_preflight_check $CLOUD_KEY $CONFIG_FILE || exit 1
 
-print_process_start "Pre-creating topics"
+printf "\n";print_process_start "Pre-creating topics"
 
 CMD="ccloud kafka topic create _confluent-monitoring"
 $CMD \
@@ -72,7 +85,7 @@ $CMD \
 print_pass "Pre-created topics"
  
 printf "\nStarting local connect cluster in Docker to generate simulated data\n"
-docker-compose up -d connect > /dev/null 2>&1 \
+(docker-compose up -d connect | tee -a $LOG_FILE) \
   && print_code_pass -c "docker-compose up -d connect" \
   || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -2))
 
@@ -84,10 +97,10 @@ retry $MAX_WAIT check_connect_up connect || exit 1
 sleep 2 # give connect an exta moment to fully mature
 print_pass "Kafka Connect has started"
  
-# Configure datagen connectors
-source ./connectors/submit_datagen_pageviews_config_cloud.sh >/dev/null 2>&1
-source ./connectors/submit_datagen_users_config_cloud.sh >/dev/null 2>&1
-
+printf "\n";print_process_start "Starting kafka-connect-datagen connectors to produce sample data."
+printf "\tSee https://www.confluent.io/hub/confluentinc/kafka-connect-datagen for more information\n"
+source ./connectors/submit_datagen_pageviews_config_cloud.sh >> $LOG_FILE 2>&1
+source ./connectors/submit_datagen_users_config_cloud.sh >> $LOG_FILE 2>&1
 printf "\nSleeping 30 seconds to give kafka-connect-datagen a chance to start producing messages\n"
 sleep 30
 
@@ -106,8 +119,8 @@ $CMD \
   || exit_with_error -c $? -n "$NAME" -m "$CMD" -l $(($LINENO -3))
 
 sleep 60
-printf "\nSubmitting KSQL queries via curl to the ksqlDB REST endpoint\n\n"
-
+printf "\nSubmitting KSQL queries via curl to the ksqlDB REST endpoint\n"
+printf "\tSee https://docs.ksqldb.io/en/latest/developer-guide/api/ for more information\n"
 while read ksqlCmd; do # from docker-cloud-statements.sql
 	response=$(curl -X POST $KSQL_ENDPOINT/ksql \
 	       -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" \
@@ -130,11 +143,11 @@ EOF
 	}
 sleep 3;
 done < docker-cloud-statements.sql
-echo "-----------------------------------------------------------"
+printf "-----------------------------------------------------------\n"
 
-echo
-echo "Local client configuration file written to $CONFIG_FILE"
-echo
-echo "Confluent Cloud KSQL is running and accruing charges. To destroy this demo run and verify ->"
-echo "    ./stop-docker-cloud.sh $CONFIG_FILE"
-echo
+printf "\nLocal client configuration file written to $CONFIG_FILE\n"
+printf "Confluent Cloud KSQL is running and accruing charges. To destroy this demo run and verify ->\n"
+printf "\t./stop-docker-cloud.sh $CONFIG_FILE\n"
+
+printf "\n<<<<<<<<<< exiting %s @ %s <<<<<<<<<<\n\n" $NAME "$(date)" >> "$NAME.log"
+
